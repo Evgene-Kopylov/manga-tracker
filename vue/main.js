7d623ca45f917d8611f3912cd3e0dc5d155a57c7
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-01-14 10:35:34 +0200

    django init

---
+++ vue/main.js
@@ -0,0 +1,12 @@
+let app = Vue.createApp({
+    data: function() {
+        return {
+            greeting: 'ok',
+            product: '*****_'
+        }
+    },
+    methods: {
+    }
+})
+
+app.mount('#app')
