c5679e5dda13294aa587afb42d0ccb9525c0c139
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-01-18 09:17:16 +0200

    test add_title

---
+++ fastapi/tests/test_add_title.py
@@ -0,0 +1,17 @@
+import unittest
+
+import pytest
+import json
+
+from fastapi.testclient import TestClient
+from main import app
+
+
+class TestAddTitle(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_api(self):
+        response = self.client.get("/add_title?url=a&element=b&block=c")
+        assert json.loads(response.content.decode('utf-8')) \
+               == {"url": "a", "element": "b", "block": "c"}
