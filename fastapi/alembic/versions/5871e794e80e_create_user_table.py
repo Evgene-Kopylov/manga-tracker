f88b1c03ca4bb571c01149bac9f2128a4badf39f
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-01-20 16:35:37 +0200

    migration fix, test user

---
+++ fastapi/alembic/versions/5871e794e80e_create_user_table.py
@@ -0,0 +1,42 @@
+"""create user page page_user
+
+Revision ID: 5871e794e80e
+Revises: 
+Create Date: 2022-01-20 11:32:12.980647
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '5871e794e80e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'user',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('email', sa.String(200), nullable=False, unique=True),
+        sa.Column('username', sa.String(200))
+    )
+    op.create_table(
+        'page',
+        sa.Column('id', sa.Integer, primary_key=True, index=True),
+        sa.Column('url', sa.String(), nullable=False),
+        sa.Column('element', sa.String()),
+        sa.Column('block', sa.String()),
+    )
+    op.create_table(
+        'page_user',
+        sa.Column('page_id', sa.Integer, primary_key=True),
+        sa.Column('user_id', sa.Integer, primary_key=True)
+    )
+
+
+def downgrade():
+    op.drop_table('user')
+    op.drop_table('page')
+    op.drop_table('page_user')
--- fastapi/db/tests/test_db.py
+++ fastapi/db/tests/test_db.py
@@ -5,38 +5,39 @@ from db.session import SessionLocal
 from db.models import User
 
 
-class TestPostgreDB(unittest.TestCase):
+class TestPostgreDBSession(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.db = SessionLocal()
-        self.email = 'abra+0@cadabra.test'
+        self.session = SessionLocal()
+        self.email = '***abra+0@cadabra.test'
+        user = self.session.query(User).filter_by(email=self.email).first()
+        if not user:
+            self.test_create_user()
 
     def tearDown(self) -> None:
-        pass
+        user = self.session.query(User).filter_by(email=self.email).first()
+        if user:
+            self.test_delete_user()
 
     def test_create_user(self):
-        user = self.db.query(User).filter_by(email=self.email).first()
+        user = self.session.query(User).filter_by(email=self.email).first()
         if not user:
             user = User()
-            user.email = self.email + '123'
-            self.db.add(user)
-            self.db.commit()
-            assert user in self.db.query(User).all()
+            user.email = self.email
+            self.session.add(user)
+            self.session.commit()
+            assert user in self.session.query(User).all()
 
-    # def test_user_exists(self):
-    #     user = self.db.query(User).filter_by(email=self.email).first()
-    #     assert user
+    def test_user_exists(self):
+        user = self.session.query(User).filter_by(email=self.email).first()
+        assert user
 
-    # def test_delete_user(self):
-    #     user = self.db.query(User).filter_by(email=self.email).first()
-    #     print(user.id)
-    #     user = User.query.get(1)
-    #     print(user.id)
-        #
-        # self.db.delete(user)
-        # self.db.commit()
-        # user = self.db.query(User).filter_by(email=self.email).first()
-        # assert not user
+    def test_delete_user(self):
+        user = self.session.query(User).filter_by(email=self.email).first()
+        print(user.id)
+        print(user.email)
+        self.session.delete(user)
+        self.session.commit()
 
     def test_save_scenario(self):
         pass
