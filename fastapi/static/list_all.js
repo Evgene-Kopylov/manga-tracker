a1198111090444f3a44489af68c965a46faf0ae9
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-01-30 01:14:52 +0200

    jquery

--- fastapi/static/list_all.js
+++ fastapi/static/list_all.js
@@ -1,24 +1,26 @@
 var client_id = Date.now()
-document.querySelector("#ws-id").textContent = client_id;
+
+$("#ws-id").textContent = client_id;
 var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
 ws.onmessage = function(event) {
-    var tbody = document.getElementById('user_list')
+    var tbody = $('#user_list')
     var collection = eval(event.data)
 
     for (var i in eval(event.data)) {
         var item = collection[i]
-        var item_line = document.createElement('tr')
-        item_line.setAttribute('id', 'page_' + item.id)
+        
+        $("<tr>", {
+            id: 'page_' + item.id
+        }).prependTo("#user_list")
 
-        var column_0 = document.createElement('td')
-        column_0.innerText = item.name
-        item_line.appendChild(column_0)
+        $("<td>", {
+            text: item.name
+        }).appendTo("#page_" + item.id)
 
-        var column_1 = document.createElement('td')
-        column_1.innerText = item.chapters_total
-        item_line.appendChild(column_1)
+        $("<td>", {
+            text: item.chapters_total
+        }).appendTo("#page_" + item.id)
 
-        tbody.prepend(item_line)
     }
 };
 function sendMessage(event) {
