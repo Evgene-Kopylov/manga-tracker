542b21233ebe6a4072fc26b7c7d0a362bf6a336f
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-02-12 21:16:28 +0200

    manga_parser tests

--- manga_parser/tests/test_manga_parser.py
+++ manga_parser/tests/test_manga_parser.py
@@ -13,24 +13,25 @@ session = SessionLocal()
 
 class TestMangaParser(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.mp = MangaParser(local=True)
-        self.driver = self.mp._driver(local=True)
-        self.test_url = os.environ.get("MANGA_TRACKER_URL", 'http://127.0.0.1:8000') + '/test_page'
-
+    @classmethod
+    def setUpClass(cls):
+        cls.mp = MangaParser(local=True)
+        cls.driver = cls.mp._driver(local=True)
+        cls.test_url = os.environ.get("MANGA_TRACKER_URL", 'http://127.0.0.1:8000') + '/test_page'
         page = Page()
-        self.chapters = ['Chapter 4', 'Chapter 5']
-        page.url = self.test_url + '?chapters=' + ', '.join(self.chapters)
+        cls.chapters = ['Chapter 4', 'Chapter 5']
+        page.url = cls.test_url + '?chapters=' + ', '.join(cls.chapters)
         page.element = 'html > body > div.list > table > tbody > tr > td > h5'
         page.block = 'html > body > div.list > table'
         page.chapters = ['Chapter 1', 'Chapter 2', 'Chapter 3']
-        self.test_page = page
-        session.add(self.test_page)
+        cls.test_page = page
+        session.add(cls.test_page)
         session.commit()
 
-    def tearDown(self) -> None:
-        self.driver.quit()
-        session.delete(self.test_page)
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.quit()
+        session.delete(cls.test_page)
         session.commit()
 
     def test_driver_get(self):
@@ -59,23 +60,23 @@ class TestMangaParser(unittest.TestCase):
         session.refresh(self.test_page)
         assert self.test_page.new == 2
 
-    # @unittest.skip
-    # def test_page_soup(self):
-    #     url = 'https://murimlogin.com/'
-    #     driver = self.mp._driver()
-    #     soup = self.mp._page_soup(url, driver)
-    #     # with open('murim_login.html', 'w', encoding='utf-8') as file:
-    #     #     file.write(soup.prettify())
-    #     driver.quit()
+    @unittest.skip
+    def test_seve_soup(self):
+        url = 'https://murimlogin.com/'
+        driver = self.mp._driver()
+        soup = self.mp._page_soup(url, driver)
+        # with open('murim_login.html', 'w', encoding='utf-8') as file:
+        #     file.write(soup.prettify())
+        driver.quit()
 
-    # @unittest.skip
-    # def test_page_block(self):
-    #     # with open('murim_login.html', 'r', encoding='utf-8') as file:
-    #     #     html = file.read()
-    #     #     soup = BeautifulSoup(html, 'html.parser')
-    #     page = session.query(Page).first()
-    #     print(f"{page.block=}")
-    #     print(f'{page.element=}')
+    @unittest.skip
+    def test_save_block(self):
+        # with open('murim_login.html', 'r', encoding='utf-8') as file:
+        #     html = file.read()
+        #     soup = BeautifulSoup(html, 'html.parser')
+        page = session.query(Page).first()
+        print(f"{page.block=}")
+        print(f'{page.element=}')
 
     @unittest.skip
     def test_container_restart(self):
