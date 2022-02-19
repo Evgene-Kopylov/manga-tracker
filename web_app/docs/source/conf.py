e3daa66e4c9c46700341febb5ba25006b7c8e380
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-02-19 15:53:27 +0200

    autodocks web_app

--- web_app/docs/source/conf.py
+++ web_app/docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'death'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'
+    # 'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
