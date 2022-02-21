e859fffebd1ad64e3195e25ea489f0ca54e5af03
Evgene-Kopylov <40310109+Evgene-Kopylov@users.noreply.github.com>
2022-02-21 12:23:33 +0200

    autodocks options fix

--- web_app/docs/source/conf.py
+++ web_app/docs/source/conf.py
@@ -39,6 +39,14 @@ extensions = [
     # 'sphinx.ext.napoleon'
 ]
 
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    # 'undoc-members': True,
+    # 'exclude-members': '__weakref__'
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -58,11 +66,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    # 'undoc-members': True,
-    'exclude-members': '__weakref__'
-}
